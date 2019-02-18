2ea0e2565706f0972784132eb90e680ebd9f6f6b
Lucas <Frutamina>
2019-02-18 14:16:47 -0300

    concerto bug

--- node-str/src/routes/product-route.js
+++ node-str/src/routes/product-route.js
@@ -6,8 +6,6 @@ const router = require('./route-config')
 const controller = require('../controller/product-controller');
 
 
-router.put('/:id', controller.put);
-router.delete('/', controller.delete);
 router.get('/search/name/:name', controller.getByName);
 router.get('/search/cod/:id', controller.getById);
 router.post('/insertEstoque/:data', controller.insertEstoque)
