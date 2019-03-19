39ad73ad675053a035c60df004ae875ad2fd36e0
thiagoamaci <46783586+thiagoamaci@users.noreply.github.com>
2019-03-19 12:10:06 -0300

    Lista de Clientes

--- .history/node-str/src/controller/admin-controller_20190319083550.js
+++ .history/node-str/src/controller/admin-controller_20190319083550.js
@@ -4,12 +4,12 @@ const sql = require('../db/sqlconfig');
 const TABLE_ECONOMIC_GROUP_CLIENT_LIST = "GRUPO_ECONOMICO_CLIENTE";
 const TABLE_CLIENT_LIST = "CLIENTE_FORNECEDOR";
 
+
 exports.getEconomicGroupList = function(req, res) {
     let query ="SELECT idGrupoEconomicoCliente, nomeGrupoEconomicoCliente, idCategoriaCliente FROM " + TABLE_ECONOMIC_GROUP_CLIENT_LIST;
     sql.execSqlQueryClientReturn(query, res);
 }
-
 exports.getClientList = function(req, res) {
-let query ="SELECT * FROM " + TABLE_CLIENT_LIST + " ORDER BY CODIGO ASC";
+let query ="SELECT * FROM " + TABLE_CLIENT_LIST + " ORDER BY ID ASC";
     sql.execSqlQueryClientReturn(query, res);
 }
