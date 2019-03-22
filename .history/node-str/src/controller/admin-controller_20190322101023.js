50fb772c9122e91380d49196b701f8cbb350cdfe
thiagoamaci <46783586+thiagoamaci@users.noreply.github.com>
2019-03-22 10:10:34 -0300

    Create admin-controller_20190322101023.js

---
+++ .history/node-str/src/controller/admin-controller_20190322101023.js
@@ -0,0 +1,77 @@
+'use-strict';
+
+const sql = require('../db/sqlconfig');
+const TABLE_ECONOMIC_GROUP_CLIENT_LIST = "GRUPO_ECONOMICO_CLIENTE";
+const TABLE_CLIENT_LIST = "CLIENTE_FORNECEDOR";
+const util = require('../../utils/utils');
+
+exports.getEconomicGroupList = function(req, res) {
+    let query ="SELECT idGrupoEconomicoCliente, nomeGrupoEconomicoCliente, idCategoriaCliente FROM " + TABLE_ECONOMIC_GROUP_CLIENT_LIST;
+    sql.execSqlQueryClientReturn(query, res);
+}
+
+exports.getClientList = function(req, res) {
+    let query ="SELECT * FROM " + TABLE_CLIENT_LIST + " ORDER BY CODIGO ASC";
+    sql.execSqlQueryClientReturn(query, res);
+}
+
+exports.createNewManagerAccount = function(req, res) {
+
+    let userData = JSON.parse(req.params.data);
+
+    let nomeUsuario = userData.email;
+    let email = userData.email;
+    let loja = userData.loja;
+    let apelidoUsuario = userData.apelidoUsuario;
+    let idUsuario = userData.idUsuario
+
+    //A senha deverá ser gerada automaticamente pelo sistema
+    let senha = util.generatePassword();
+    let idCargo = 2;
+    let ativo = 1;
+    let envioDiario = 1;
+
+    if(email != null && email!= undefined && email != ""){
+
+        let query = "INSERT INTO PRE_USUARIO (nomeUsuario, email, senha, loja, idCargo, ativo, apelidoUsuario, envioDiario) VALUES ('"+nomeUsuario+"', '"+email+"', '"+senha+"', "+loja+", "+idCargo+", "+ativo+", '"+apelidoUsuario+"', "+envioDiario+")";
+
+        console.log("userData : " + senha);
+
+        sql.execSqlQuery(query, res).then(() => {
+            res.json(util.jsonStatusReturn['success'])
+        });
+
+    }else{
+        return res.json(util.jsonStatusReturn['error']);
+    }
+}
+exports.createNewUserAccount = function(req, res) {
+
+    let userData = JSON.parse(req.params.data);
+
+    let nomeUsuario = userData.email;
+    let email = userData.email;
+    let loja = userData.loja;
+    let apelidoUsuario = userData.apelidoUsuario;
+    let idUsuario = userData.idUsuario
+
+    //A senha deverá ser gerada automaticamente pelo sistema
+    let senha = util.generatePassword();
+    let idCargo = 1;
+    let ativo = 1;
+    let envioDiario = 1;
+
+    if(email != null && email!= undefined && email != ""){
+
+        let query = "INSERT INTO PRE_USUARIO (nomeUsuario, email, senha, loja, idCargo, ativo, apelidoUsuario, envioDiario) VALUES ('"+nomeUsuario+"', '"+email+"', '"+senha+"', "+loja+", "+idCargo+", "+ativo+", '"+apelidoUsuario+"', "+envioDiario+")";
+
+        console.log("userData : " + senha);
+
+        sql.execSqlQuery(query, res).then(() => {
+            res.json(util.jsonStatusReturn['success'])
+        });
+
+    }else{
+        return res.json(util.jsonStatusReturn['error']);
+    }
+}
